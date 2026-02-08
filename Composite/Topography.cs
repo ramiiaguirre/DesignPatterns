7a2a9fb2b647c32138098d344b74dfe38d204b1e
Ramiro Aguirre <80402903+ramiiaguirre@users.noreply.github.com>
2026-02-08 17:22:29 -0300

    Update Topography.cs

--- Composite/Topography.cs
+++ Composite/Topography.cs
@@ -1,5 +1,11 @@
 namespace Composite;
 public abstract class Topography 
 {
-	public abstract double getWaterProportion();	
+	public abstract double GetWaterProportion();
+	public double GetLandProportion()
+	{
+		return 1 - this.GetWaterProportion();
+	}
+
+	//code equals
 }
